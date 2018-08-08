0fee23a073421927815809e697c86f181a2bbabd
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-08 13:14:39 -0700

    show after load

--- electron/starter.js
+++ electron/starter.js
@@ -3,7 +3,11 @@ const path = require('path')
 const url = require('url')
 let mainWindow
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        show: false
+    })
     mainWindow.loadURL(process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
         protocol: 'file:',
