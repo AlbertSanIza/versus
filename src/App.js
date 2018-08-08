7d76a7f1e3c610e3778f1e1058a24355721eae7c
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-08 16:55:47 -0700

    send some messages over socket io

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
-import openSocket from 'socket.io-client';
+import io from 'socket.io-client'
 import logo from './logo.svg'
 import './App.css'
 
-const socket = openSocket('http://localhost:12345')
+const socket = io('http://localhost:12345')
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        socket.on('message', msg => {
+            console.log(msg)
+        })
+    }
+    handleClick = () => {
+        socket.emit('message', {message: "test"})
+    }
     render() {
         return (
             <div className="App">
@@ -16,6 +25,8 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
+                <br/>
+                <button onClick={this.handleClick}>TEST</button>
             </div>
         )
     }
