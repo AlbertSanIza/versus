5b61e32518857113b912991adf5280b7f6c7fd0a
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-07 22:48:42 -0700

    run on enviroment

--- electron/starter.js
+++ electron/starter.js
@@ -2,7 +2,11 @@ const { app, BrowserWindow } = require('electron')
 let mainWindow
 function createWindow () {
     mainWindow = new BrowserWindow({width: 800, height: 600})
-    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.loadURL(process.env.ELECTRON_START_URL || url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
     mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function () {
         mainWindow = null
