b0d167fe17707c2d48106efb45f67c5a03c47a68
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-09 10:33:47 -0700

    get server working

--- electron/starter.js
+++ electron/starter.js
@@ -2,6 +2,10 @@ const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
 let mainWindow
+var express = require('express')
+var expressApp = express()
+var http = require('http').Server(expressApp).listen(12345)
+var io = require('socket.io')(http)
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 800,
@@ -20,6 +24,19 @@ app.on('ready', () => {
     mainWindow.webContents.on('did-finish-load', () => {
         mainWindow.show()
     })
+    expressApp.use('/static', express.static(__dirname + '/../build/static'))
+    expressApp.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '/../build/index.html'))
+    })
+    io.on('connection', socket => {
+        console.log('User Connected')
+        socket.on('disconnect', () => {
+            console.log('User Disconnected')
+        })
+        socket.on('message', msg => {
+            io.emit('message', msg)
+        })
+    })
 })
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
@@ -30,23 +47,4 @@ app.on('activate', () => {
     if (mainWindow === null) {
         createWindow()
     }
-})
-var express = require('express')()
-var http = require('http').Server(express)
-var io = require('socket.io')(http)
-var port = 12345
-express.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/../build/index.html'))
-})
-io.on('connection', socket => {
-    console.log('a user connected')
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
-    })
-    socket.on('message', msg => {
-        io.emit('message', msg)
-    })
-})
-http.listen(port, () => {
-    console.log('listening on *:' + port)
 })
