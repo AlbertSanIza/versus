f9fd5adc1b2f5a109d2e974b9f1539b2788b67bd
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-09 21:27:33 -0700

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 import io from 'socket.io-client'
-import logo from './logo.svg'
-import './App.css'
 
 const socket = io('http://' + window.location.hostname + ':12345')
 
@@ -20,17 +18,7 @@ class App extends Component {
         return (
             <HashRouter>
                 <div>
-                    <div className="App">
-                        <header className="App-header">
-                            <img src={logo} className="App-logo" alt="logo" />
-                            <h1 className="App-title">Welcome to React: {window.location.hostname}</h1>
-                        </header>
-                        <p className="App-intro">
-                            To get started, edit <code>src/App.js</code> and save to reload.
-                        </p>
-                        <br/>
-                        <button onClick={this.handleClick}>TEST</button>
-                    </div>
+                    <button onClick={this.handleClick}>TEST</button>
                 </div>
             </HashRouter>
         )
