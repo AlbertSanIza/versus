57ebf0e96fc396f622f7844432062be76a907795
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-10 10:50:11 -0700

    show some stuff

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 class Main extends Component {
     render() {
         return(
-            <div>Main</div>
+            <div>
+                <AppBar position="static" color="secondary">
+                    <Toolbar variant="dense">
+                        <IconButton color="inherit" aria-label="Menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="title" color="inherit">Versus</Typography>
+                    </Toolbar>
+                </AppBar>
+                <Button variant="contained" color="primary">TEST</Button>
+            </div>
         )
     }
 }
