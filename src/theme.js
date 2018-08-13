5866511a66f665e9d10363a66f1366a38063a85c
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-13 16:57:51 -0700

    update palette

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,14 @@
 import { createMuiTheme } from '@material-ui/core/styles'
+import grey from '@material-ui/core/colors/grey'
 import red from '@material-ui/core/colors/red'
-import green from '@material-ui/core/colors/green'
 
 export default createMuiTheme({
     palette: {
-        primary: red,
-        secondary: green
+        primary: {
+            main: grey[900]
+        },
+        secondary: {
+            main: red[600]
+        }
     }
 })
