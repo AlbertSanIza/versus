c7588919de1f7fdcc10a5ca79a28469de5db339d
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-13 16:31:54 -0700

    allow theming

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import theme from './theme'
 import 'typeface-roboto'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <App/>
+    </MuiThemeProvider>,
+    document.getElementById('root')
+)
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import red from '@material-ui/core/colors/red'
+import green from '@material-ui/core/colors/green'
+
+export default createMuiTheme({
+    palette: {
+        primary: red,
+        secondary: green
+    }
+})
