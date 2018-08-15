029c3ab3a26ec7919957b9f4ccab6428b4221b5d
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-14 18:11:24 -0700

    started drawer

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,52 +1,57 @@
 import React, { Component } from 'react'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
+import Drawer from '@material-ui/core/Drawer'
 import Card from '@material-ui/core/Card'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+https://material-ui.com/demos/drawers/
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+        height: 100,
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: 240,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        minWidth: 0
+    },
+    toolbar: theme.mixins.toolbar,
+})
 
 class Main extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            open: false
-        }
-    }
-    handleDrawerOpen = () => {
-        this.setState({ open: true })
-    }
-    handleDrawerClose = () => {
-        this.setState({ open: false })
-    }
     render() {
         return(
-            <React.Fragment>
-                <AppBar position="static" color="primary">
-                    <Toolbar variant="dense">
-                        <Typography variant="title" color="inherit">Versus</Typography>
+            <div className={ this.props.classes.root }>
+                <AppBar position="absolute" className={ this.props.classes.appBar }>
+                    <Toolbar>
+                        <Typography variant="title" color="inherit" noWrap>Versus</Typography>
                     </Toolbar>
                 </AppBar>
-                <Grid container spacing={16} style={{ margin: 0, width: '100%'}}>
-                    <Grid item xs={4}>
-                        <Card>
-                            <CardContent>
-                                <Button variant="contained" color="secondary">TEST</Button>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Card>
-                            <CardContent>
-                                TEST
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                </Grid>
-            </React.Fragment>
+                <Drawer variant="permanent" className="drawerPaper" classes={{ paper: this.props.classes.drawerPaper }}>
+                </Drawer>
+                <main className={ this.props.classes.content }>
+                    <Typography noWrap>{'You think water moves fast? You should see ice.'}</Typography>
+                </main>
+            </div>
         )
     }
 }
 
-export default Main
+export default withStyles(styles)(Main)
