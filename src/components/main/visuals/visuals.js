5c2d1f566a8ded7833055a3ce4584ea556618900
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-15 18:02:09 -0700

    show versus viewer

--- src/components/main/visuals/visuals.js
+++ src/components/main/visuals/visuals.js
@@ -5,12 +5,14 @@ import Typography from '@material-ui/core/Typography'
 import Input from '@material-ui/core/Input'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
+import VersusViewer from '../versusViewer/'
 
 class Competitors extends Component {
     render() {
         return(
             <React.Fragment>
                 <Typography variant="display2" gutterBottom>Visuales</Typography>
+                <VersusViewer></VersusViewer>
             </React.Fragment>
         )
     }
