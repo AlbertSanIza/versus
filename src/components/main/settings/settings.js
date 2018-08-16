9a82086d69fc5e9f695d1c32623beed9850463a8
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-16 11:33:20 -0700

    add logos and links

--- src/components/main/settings/settings.js
+++ src/components/main/settings/settings.js
@@ -3,22 +3,25 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import instagramLogo from './instagramLogo.svg'
 import githubLogo from './githubLogo.svg'
+import './settings.css'
 
 class Settings extends Component {
     render() {
         return (
             <React.Fragment>
                 <Typography variant="display2" gutterBottom>Ajustes</Typography>
+                <Typography variant="body1" align="center">Creado por Albert Sanchez</Typography>
                 <Typography variant="body1" align="center">
-                    <Button href="https://github.com/AlbertSanIza">
-                        Creado por Albert Sanchez
-                    </Button>
-                    <a href="https://github.com/AlbertSanIza" target="_blank" rel="noopener noreferrer">Albert Sanchez</a>
-                <br/>
-            </Typography>
-        </React.Fragment>
-    )
-}
+                    <a href="https://www.instagram.com/albertsaniza/" target="_blank" rel="noopener noreferrer">
+                        <img className="settings-instagram-logo" src={ instagramLogo } alt=""/>
+                    </a>
+                    <a href="https://github.com/BatallaDeMaestros" target="_blank" rel="noopener noreferrer">
+                        <img className="settings-github-logo" src={ githubLogo } alt=""/>
+                    </a>
+                </Typography>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Settings
