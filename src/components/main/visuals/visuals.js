9bb35f2b7461c2907ec4c6eb5fdc8922c0aba3b8
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-15 22:49:39 -0700

    never used

--- src/components/main/visuals/visuals.js
+++ src/components/main/visuals/visuals.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import FormControl from '@material-ui/core/FormControl'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import Input from '@material-ui/core/Input'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import VersusViewer from '../versusViewer/'
