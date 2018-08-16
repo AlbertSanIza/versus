80c386b8a83623b4bf92fa48dfaf46aee176dc4e
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-15 22:37:14 -0700

    autoindent

--- src/components/main/settings/settings.js
+++ src/components/main/settings/settings.js
@@ -5,23 +5,24 @@ import githubLogo from './githubLogo.svg'
 
 class Settings extends Component {
     render() {
-        return(
+        return (
             <React.Fragment>
                 <Typography variant="display2" gutterBottom>Ajustes</Typography>
                 <div>
                     <hr/>
-                    Creado por <a href="https://github.com/AlbertSanIza" target="_blank" rel="noopener noreferrer">Albert Sanchez</a>
-                <br/>
-                <a href="https://www.instagram.com/albertsaniza/" target="_blank" rel="noopener noreferrer">
-                    <img className="logo" src={instagramLogo} alt=""/>
-                </a>
-                <a href="https://github.com/BatallaDeMaestros" target="_blank" rel="noopener noreferrer">
-                    <img className="logo" src={githubLogo} alt=""/>
-                </a>
-            </div>
-        </React.Fragment>
-    )
-}
+                    Creado por
+                    <a href="https://github.com/AlbertSanIza" target="_blank" rel="noopener noreferrer">Albert Sanchez</a>
+                    <br/>
+                    <a href="https://www.instagram.com/albertsaniza/" target="_blank" rel="noopener noreferrer">
+                        <img className="logo" src={instagramLogo} alt=""/>
+                    </a>
+                    <a href="https://github.com/BatallaDeMaestros" target="_blank" rel="noopener noreferrer">
+                        <img className="logo" src={githubLogo} alt=""/>
+                    </a>
+                </div>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Settings
