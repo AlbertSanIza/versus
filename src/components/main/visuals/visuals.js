bbcfd041ddb5c868fa3f21f3600b1b001aaf571e
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-17 13:53:07 -0700

    put in card instead

--- src/components/main/visuals/visuals.js
+++ src/components/main/visuals/visuals.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import VersusViewer from '../versusViewer/'
@@ -19,11 +20,9 @@ class Competitors extends Component {
                         </Card>
                     </Grid>
                     <Grid item sm={ 8 }>
-                        <Card>
-                            <CardContent>
-                                <VersusViewer></VersusViewer>
-                            </CardContent>
-                        </Card>
+                        <Paper>
+                            <VersusViewer></VersusViewer>
+                        </Paper>
                     </Grid>
                 </Grid>
             </React.Fragment>
