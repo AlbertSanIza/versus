017ee8555cda0cd7f2803f9bdcc7484dc8991953
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-18 12:17:42 -0700

    add redux reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import 'typeface-roboto'
 import './index.css'
+import rootReducer from './reducers'
 import theme from './theme'
 import App from './App'
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-    <MuiThemeProvider theme={ theme }>
-        <App/>
-    </MuiThemeProvider>,
+    <Provider store={store}>
+        <MuiThemeProvider theme={ theme }>
+            <App/>
+        </MuiThemeProvider>
+    </Provider>,
     document.getElementById('root')
 )
