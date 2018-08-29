b76aeb19e2242a89f90aff13f26627b6fa70262c
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-29 15:59:20 -0700

    re aranged components

--- src/components/main/visuals/visuals.js
+++ src/components/main/visuals/visuals.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react'
-import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
 import './visuals.css'
 
 class Competitors extends Component {
     render() {
         return(
             <React.Fragment>
-                <Typography variant="display2" gutterBottom>Visuales</Typography>
+                <div className="visuals-iframe-holder">
+                    <iframe src="#/" frameBorder="0" title="visualizer" className="visuals-iframe"></iframe>
+                </div>
+                <br/>
                 <Grid container spacing={ 16 }>
                     <Grid item sm={ 4 }>
-                        <Card>
-                            <CardContent>
-                                <Typography variant="title">Controles</Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                    <Grid item sm={ 8 }>
-                        <Paper>
-                            <iframe src="#/" frameBorder="0" title="visualizer" className="visuals-iframe"></iframe>
-                        </Paper>
+                        <Typography variant="title">Controles</Typography>
                     </Grid>
                 </Grid>
             </React.Fragment>
