bf3d2beb1069007ae194a0dc3fff8aed2ce51380
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-29 16:22:23 -0700

    add searchbars to all

--- src/components/main/events/events.js
+++ src/components/main/events/events.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import FormControl from '@material-ui/core/FormControl'
 import Typography from '@material-ui/core/Typography'
+import Input from '@material-ui/core/Input'
+import Grid from '@material-ui/core/Grid'
 
 class Event extends Component {
     render() {
+        const {
+            events,
+            searchTerm,
+            searchEvents
+        } = this.props
         return(
             <React.Fragment>
                 <Typography variant="display2" gutterBottom>Eventos</Typography>
+                <Grid container spacing={ 16 }>
+                    <Grid item sm={ 12 }>
+                        <FormControl fullWidth>
+                            <Input placeholder="Busqueda" value={ searchTerm } onChange={ e => searchEvents(e.target.value) }></Input>
+                        </FormControl>
+                    </Grid>
+                </Grid>
             </React.Fragment>
         )
     }
--- src/components/main/thematics/thematics.js
+++ src/components/main/thematics/thematics.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import FormControl from '@material-ui/core/FormControl'
 import Typography from '@material-ui/core/Typography'
+import Input from '@material-ui/core/Input'
+import Grid from '@material-ui/core/Grid'
 
 class Thematics extends Component {
     render() {
+        const {
+            thematics,
+            searchTerm,
+            searchThematics
+        } = this.props
         return (
             <React.Fragment>
                 <Typography variant="display2" gutterBottom>Tematicas</Typography>
+                <Grid container spacing={ 16 }>
+                    <Grid item sm={ 12 }>
+                        <FormControl fullWidth>
+                            <Input placeholder="Busqueda" value={ searchTerm } onChange={ e => searchThematics(e.target.value) }></Input>
+                        </FormControl>
+                    </Grid>
+                </Grid>
             </React.Fragment>
         )
     }
