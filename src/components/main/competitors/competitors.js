d0de6c7ed85855984fbb11f8fd3f612d6f3d2034
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-29 22:14:52 -0700

    fixed height and no wrap

--- src/components/main/competitors/competitors.js
+++ src/components/main/competitors/competitors.js
@@ -10,7 +10,8 @@ import Card from '@material-ui/core/Card'
 
 const styles = {
     media: {
-        height: '18vh'
+        height: 0,
+        paddingTop: '80%'
     }
 }
 
@@ -27,11 +28,11 @@ class Competitors extends Component {
                         </FormControl>
                     </Grid>
                     { competitors.map((competitor, i) => (
-                        <Grid item xs={ 12 } sm={ 4 } md={ 3 } lg={ 2 } key={ i }>
+                        <Grid item xs={ 12 } sm={ 6 } md={ 4 } lg={ 3 } xl={ 2 } key={ i }>
                             <Card>
                                 <CardMedia className={ classes.media } image={ competitor.photo } title="Contemplative Reptile"/>
                                 <CardContent>
-                                    <Typography>{ competitor.name }</Typography>
+                                    <Typography variant="title" noWrap>{ competitor.name }</Typography>
                                 </CardContent>
                             </Card>
                         </Grid>
