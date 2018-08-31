cac9763bf50592b3e87bf4fae04437cb295386b0
Albert Sanchez <albert.san.iza@gmail.com>
2018-08-31 11:33:48 -0700

    remove old socket

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react'
 import { HashRouter, Route } from 'react-router-dom'
-import io from 'socket.io-client'
 import { Main, Visualizer } from './components'
 
-const socket = io('http://' + window.location.hostname + ':12345')
-
 class App extends Component {
-    constructor(props) {
-        super(props)
-        socket.on('message', msg => {
-            console.log(msg)
-        })
-    }
-    handleClick = () => {
-        socket.emit('message', { message: "test" })
-    }
     render() {
         return (
             <HashRouter>
