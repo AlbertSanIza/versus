1ebf9b73c2ddbf93ad79a5cde8e735a5a4a4feee
Albert Sanchez <albert.san.iza@gmail.com>
2018-09-01 00:08:35 -0700

    add old controlers

--- src/components/main/visuals/visuals.js
+++ src/components/main/visuals/visuals.js
@@ -32,6 +32,41 @@ const styles = theme => ({
 })
 
 class Competitors extends Component {
+    state = {
+        status: "set",
+        seconds: "",
+        text: ""
+    }
+    inputHandleChange = e => {
+        this.setState({ seconds: e.target.value })
+    }
+    inputTextHandleChange = e => {
+        this.setState({ text: e.target.value })
+    }
+    setButton = () => {
+        this.setState({ status: "isSet" })
+        this.props.visualizer({ status: "isSet", seconds: this.state.seconds, text: this.state.text })
+    }
+    startButton = () => {
+        this.setState({status: "isStart"})
+        this.props.visualizer({ status: "isStart", seconds: this.state.seconds, text: this.state.text})
+    }
+    pauseButton = () => {
+        this.setState({status: "isSet"})
+        this.props.visualizer({ status: "isPause", seconds: this.state.seconds, text: this.state.text})
+    }
+    resetButton = () => {
+        this.setState({status: "set", seconds: "", text: ""})
+        this.props.visualizer({ status: "isReset", seconds: "0", text: this.state.text})
+    }
+    onTick = () => {
+        this.setState({seconds: this.state.seconds - 1})
+        this.props.visualizer({ status: "isStart", seconds: this.state.seconds, text: this.state.text})
+    }
+    onComplete = () => {
+        this.setState({status: "set", seconds: ""})
+        this.props.visualizer({ status: "isReset", seconds: "0"})
+    }
     render() {
         const { classes } = this.props
         return(
@@ -45,6 +80,65 @@ class Competitors extends Component {
                         <Typography variant="title">Controles</Typography>
                     </Grid>
                 </Grid>
+                <div className="card border-dark">
+                    <div className="card-header">
+                        <b>
+                            Controles
+                            {(() => {
+                                switch (this.state.status) {
+                                    case "isStart":
+                                    return (
+                                        <React.Fragment>
+                                            <div className="countdown">
+                                                <Countdown date={Date.now() + (this.state.seconds * 1000)} onTick={this.onTick} onComplete={this.onComplete} renderer={props => <React.Fragment>{props.total / 1000}</React.Fragment>}/>
+                                            </div>
+                                        </React.Fragment>
+                                    )
+                                    default:
+                                    return ""
+                                }
+                            })()}
+                        </b>
+                    </div>
+                    <div className="card-body">
+                        <input className="form-control" type="text" placeholder="Tema" disabled={this.state.status !== "set"} value={this.state.text} onChange={this.inputTextHandleChange}/>
+                        <br/>
+                        <div className="input-group input-group-sm mb-3">
+                            <input className="form-control" type="number" placeholder="Tiempo" min="5" disabled={this.state.status !== "set"} value={this.state.seconds} onChange={this.inputHandleChange}/>
+                            <div className="input-group-append">
+                                <span className="input-group-text">Segundos</span>
+                            </div>
+                        </div>
+                        {(() => {
+                            switch (this.state.status) {
+                                case "set":
+                                return (
+                                    <button className="btn btn-outline-primary btn-sm btn-block" type="button" onClick={this.setButton} disabled={!this.state.seconds || this.state.seconds < 5}>Mostrar</button>
+                                )
+                                case "isSet":
+                                case "isPause":
+                                return (
+                                    <div className="row">
+                                        <div className="col">
+                                            <button className="btn btn-outline-danger btn-sm btn-block" type="button" onClick={this.resetButton}>Cancelar</button>
+                                        </div>
+                                        <div className="col">
+                                            <button className="btn btn-outline-success btn-sm btn-block" type="button" onClick={this.startButton}>Iniciar</button>
+                                        </div>
+                                    </div>
+                                )
+                                case "isStart":
+                                return (
+                                    <div>
+                                        <button className="btn btn-outline-warning btn-sm btn-block" type="button" onClick={this.pauseButton}>Puasar</button>
+                                    </div>
+                                )
+                                default:
+                                return null
+                            }
+                        })()}
+                    </div>
+                </div>
                 <Grow in={ true } timeout={ 500 }>
                     <a href="#/" target="_blank" rel="noopener noreferrer">
                         <Button variant="fab" className={ classes.fab } onClick={ this.handleOpenCreate } color="primary">
