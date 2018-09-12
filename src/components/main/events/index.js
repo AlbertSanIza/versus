7984169faeae730c065f5b5dd7b1baec3a7f69de
Albert Sanchez <albert.san.iza@gmail.com>
2018-09-11 18:02:47 -0700

    remove redux from index

--- src/components/main/events/index.js
+++ src/components/main/events/index.js
@@ -1,21 +1,2 @@
-import { connect } from 'react-redux'
 import Events from './events'
-
-function searchEvents(searchTerm) {
-    return {
-        type: 'SEARCH_EVENTS',
-        payload: { searchTerm }
-    }
-}
-
-function createEvent(event) {
-    return {
-        type: 'CREATE_EVENT',
-        payload: { event }
-    }
-}
-
-export default connect(store => store.events, {
-    searchEvents,
-    createEvent
-})(Events)
+export { Events as default }
