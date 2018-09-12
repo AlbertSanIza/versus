c911467522c2e018b7167674cfadae1b9aab7599
Albert Sanchez <albert.san.iza@gmail.com>
2018-09-11 17:12:53 -0700

    remove redux trace

--- src/components/main/settings/index.js
+++ src/components/main/settings/index.js
@@ -1,12 +1,2 @@
-import { connect } from 'react-redux'
 import Settings from './settings'
-
-function openTempDir() {
-    return {
-        type: 'OPEN_TEMP_DIR'
-    }
-}
-
-export default connect(store => store.settings, {
-    openTempDir
-})(Settings)
+export { Settings as default }
