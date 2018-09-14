ecf93c65d7c4e7ee908d2c91fdae94429a61674a
Albert Sanchez <albert.san.iza@gmail.com>
2018-09-14 12:42:19 -0700

    show chaging icon

--- src/components/main/VersusAppBar/VersusAppBar.js
+++ src/components/main/VersusAppBar/VersusAppBar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import { withStyles } from '@material-ui/core/styles'
 import Toolbar from '@material-ui/core/Toolbar'
-import MenuIcon from '@material-ui/icons/Menu'
 import AppBar from '@material-ui/core/AppBar'
 
 import { withVersus } from '../../../context'
@@ -30,7 +31,9 @@ class VersusAppBar extends Component {
                 <AppBar>
                     <Toolbar color="inherit" variant="dense">
                         <IconButton className={ classes.menuButton } color="inherit" onClick={ () => Versus.handleDrawer() }>
-                            <MenuIcon/>
+                          { (() => {
+                            return Versus.drawer ? (<MoreVertIcon/>) : (<MoreHorizIcon/>)
+                          })() }
                         </IconButton>
                         <Typography className={ classes.title } variant="title" color="inherit">Versus</Typography>
                     </Toolbar>
