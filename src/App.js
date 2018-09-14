26472b761a46e1e41ccd4ac8afc65ace5cd032e2
Albert Sanchez <albert.san.iza@gmail.com>
2018-09-14 11:24:40 -0700

    wrap with VersusContext

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 import { HashRouter, Route } from 'react-router-dom'
 
 import { Main, VersusVisualizer } from './components'
-import { SocketIOProvider } from './context'
+import { VersusProvider, SocketIOProvider } from './context'
 
 class App extends Component {
     render() {
         return (
             <HashRouter>
                 <React.Fragment>
-                    <SocketIOProvider>
-                        <Route path="/main" component={ Main }/>
-                    </SocketIOProvider>
+                    <VersusProvider>
+                        <SocketIOProvider>
+                            <Route path="/main" component={ Main }/>
+                        </SocketIOProvider>
+                    </VersusProvider>
                     <Route exact path="/" component={ VersusVisualizer }/>
                 </React.Fragment>
             </HashRouter>
