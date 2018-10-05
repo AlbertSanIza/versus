0f2ac0c8ea3a610b0f5b8a6917fbf03661a31722
Albert Sanchez <albert.san.iza@gmail.com>
2018-10-04 23:34:27 -0700

    moved particles to css

--- src/components/VersusVisualizer/VersusVisualizer.js
+++ src/components/VersusVisualizer/VersusVisualizer.js
@@ -10,15 +10,6 @@ import './glitch.css'
 const socket = io('http://' + window.location.hostname + ':12345')
 
 const styles = theme => ({
-    particles: {
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        top: 0,
-        left: 0,
-        zIndex: 0,
-        overflow: 'hidden'
-    },
     visualizer: {
         position: 'absolute',
         width: '100%',
