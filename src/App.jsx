cb27f79a85c76be7981132e85636e9cf30a28aa3
Albert Sanchez <albert.san.iza@gmail.com>
2019-03-14 14:42:46 -0700

    lint

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
 import { VersusMain, VersusUpload, VersusVisualizer } from './components';
 import { VersusProvider, SocketIOProvider } from './context';
 
-class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <React.Fragment>
-          <VersusProvider>
-            <SocketIOProvider>
-              <Route path="/main" component={VersusMain} />
-              <Route exact path="/upload" component={VersusUpload} />
-            </SocketIOProvider>
-          </VersusProvider>
-          <Route exact path="/" component={VersusVisualizer} />
-        </React.Fragment>
-      </HashRouter>
-    );
-  }
+function App(props) {
+  return (
+    <HashRouter>
+      <React.Fragment>
+        <VersusProvider>
+          <SocketIOProvider>
+            <Route path="/main" component={VersusMain} />
+            <Route exact path="/upload" component={VersusUpload} />
+          </SocketIOProvider>
+        </VersusProvider>
+        <Route exact path="/" component={VersusVisualizer} />
+      </React.Fragment>
+    </HashRouter>
+  );
 }
 
 export default App;
