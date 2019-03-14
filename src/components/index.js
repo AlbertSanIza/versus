cecff6fab45ecdb49075fb3081b6ead1bfa1f237
Albert Sanchez <albert.san.iza@gmail.com>
2019-03-14 16:34:21 -0700

    lint

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
-export { default as VersusMain } from './VersusMain'
-export { default as VersusUpload } from './VersusUpload'
-export { default as VersusVisualizer } from './VersusVisualizer'
+export { default as VersusMain } from './VersusMain';
+export { default as VersusUpload } from './VersusUpload';
+export { default as VersusVisualizer } from './VersusVisualizer';
