6538fdb3e792dfa64d6169e20a3c97cceae017bc
Albert Sanchez <albert.san.iza@gmail.com>
2019-03-14 14:50:07 -0700

    lint

--- src/theme.jsx
+++ src/theme.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import { createMuiTheme } from '@material-ui/core/styles'
-import { withStyles } from '@material-ui/core/styles'
-import grey from '@material-ui/core/colors/grey'
-import red from '@material-ui/core/colors/red'
+import React from 'react';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme, withStyles } from '@material-ui/core/styles';
+import grey from '@material-ui/core/colors/grey';
+import red from '@material-ui/core/colors/red';
+import PropTypes from 'prop-types';
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: grey[900]
+      main: grey[900],
     },
-    secondary: red
-  }
-})
+    secondary: red,
+  },
+});
 
 const styles = () => ({
   '@global': {
@@ -25,38 +25,40 @@ const styles = () => ({
       '-ms-user-select': 'none',
       'user-select': 'none',
       display: 'block',
-      margin: 0
+      margin: 0,
     },
     img: {
       '-webkit-user-drag': 'none',
       '-khtml-user-drag': 'none',
       '-moz-user-drag': 'none',
       '-o-user-drag': 'none',
-      userDrag: 'none'
+      userDrag: 'none',
     },
     '::-webkit-scrollbar': {
-      width: 7
+      width: 7,
     },
     '::-webkit-scrollbar-track': {
-      background: '#f1f1f1'
+      background: '#f1f1f1',
     },
     '::-webkit-scrollbar-thumb': {
-      background: '#888'
+      background: '#888',
     },
     '::-webkit-scrollbar-thumb:hover': {
-      background: '#555'
-    }
-  }
-})
+      background: '#555',
+    },
+  },
+});
 
-class Theme extends Component {
-  render() {
-    return (
-      <MuiThemeProvider theme={ theme }>
-        { this.props.children }
-      </MuiThemeProvider>
-    )
-  }
+function Theme(props) {
+  return (
+    <MuiThemeProvider theme={theme}>
+      { props.children }
+    </MuiThemeProvider>
+  );
 }
 
-export default withStyles(styles)(Theme)
+Theme.propTypes = {
+  children: PropTypes.shape().isRequired,
+};
+
+export default withStyles(styles)(Theme);
