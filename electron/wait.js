19842b6b114072e930f66e368e0895294e608acb
Albert Sanchez <albert.san.iza@gmail.com>
2019-03-15 12:28:50 -0700

    Lint Wait.js

--- electron/wait.js
+++ electron/wait.js
@@ -1,21 +1,21 @@
-const net = require('net')
-const port = process.env.PORT ? (process.env.PORT - 100) : 3000
+const net = require('net');
+const exec = require('child_process').exec;
 
-const client = new net.Socket()
+const client = new net.Socket();
+const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
 
-let startedElectron = false
-const tryConnection = () => client.connect({port: port}, () => {
-    client.end()
-    if(!startedElectron) {
-        console.log('starting electron')
-        startedElectron = true
-        const exec = require('child_process').exec
-        exec('npm run electron')
-    }
-})
+let startedElectron = false;
+const tryConnection = () => client.connect({ port: port }, () => {
+  client.end();
+  if (!startedElectron) {
+    console.log('Starting Electron');
+    startedElectron = true;
+    exec('npm run electron');
+  }
+});
 
-tryConnection()
+tryConnection();
 
-client.on('error', (error) => {
-    setTimeout(tryConnection, 1000)
-})
+client.on('error', error => {
+  setTimeout(tryConnection, 1000);
+});
