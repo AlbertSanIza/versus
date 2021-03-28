7fe9fdfb45922b366623203b03029cfee5f562a1
Albert Sanchez <albert.san.iza@gmail.com>
2021-03-28 02:57:19 -0700

    Update VersusUpload.jsx

--- src/components/VersusUpload/VersusUpload.jsx
+++ src/components/VersusUpload/VersusUpload.jsx
@@ -32,14 +32,14 @@ class Main extends Component {
     };
   }
 
-  createTermChanged(input) {
-    this.setState({ createName: input });
-  }
-
   handleCreate() {
     const { createName, createImage } = this.state;
     this.props.SocketIO.competitors.create({ name: createName, photo: `${createName.toLowerCase().replace(/[\W_]+/g, '_') + Date.now()}.${createImage.file.type.split('/').pop()}`, file: createImage.file });
     this.setState({ createName: '', createImage: {} });
+  }
+
+  createTermChanged(input) {
+    this.setState({ createName: input });
   }
 
   render() {
