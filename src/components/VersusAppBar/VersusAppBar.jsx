fae2798b5d7d260eff7f81c5ec64b2d221ce08bd
Albert Sanchez <albert.san.iza@gmail.com>
2021-03-28 04:01:25 -0700

    clean

--- src/components/VersusAppBar/VersusAppBar.jsx
+++ src/components/VersusAppBar/VersusAppBar.jsx
@@ -3,14 +3,13 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import PropTypes from 'prop-types';
 
 import { withVersus } from '../../context';
 
-const styles = () => ({
+const style = {
   title: {
     flexGrow: 1,
     textAlign: 'center',
@@ -22,30 +21,33 @@ const styles = () => ({
   topPadding: {
     minHeight: 48,
   },
-});
+};
 
 function VersusAppBar(props) {
-  const { classes, Versus } = props;
+  const { Versus } = props;
   return (
     <>
       <AppBar>
         <Toolbar color="inherit" variant="dense">
-          <IconButton className={classes.menuButton} color="inherit" onClick={() => Versus.handleDrawer()}>
-            { (() => (Versus.drawer ? (<MoreVertIcon />) : (<MoreHorizIcon />)))() }
+          <IconButton color="inherit" style={style.menuButton} onClick={() => Versus.handleDrawer()}>
+            { Versus.drawer ? (
+              <MoreVertIcon />
+            ) : (
+              <MoreHorizIcon />
+            ) }
           </IconButton>
-          <Typography className={classes.title} variant="h6" color="inherit">
+          <Typography style={style.title} variant="h6" color="inherit">
             Versus
           </Typography>
         </Toolbar>
       </AppBar>
-      <div className={classes.topPadding} />
+      <div style={style.topPadding} />
     </>
   );
 }
 
 VersusAppBar.propTypes = {
   Versus: PropTypes.shape().isRequired,
-  classes: PropTypes.shape().isRequired,
 };
 
-export default withVersus(withStyles(styles)(VersusAppBar));
+export default withVersus(VersusAppBar);
