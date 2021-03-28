17b05a3cad8faae5914f68cc13fd5db49a6d7d4e
Albert Sanchez <albert.san.iza@gmail.com>
2021-03-28 04:29:22 -0700

    Create express.js

---
+++ scripts/express.js
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// ----------------------------------------------------------------------------
+const expressApp = express();
+const tempDir = path.join(__dirname, '/data');
+const tempImg = path.join(tempDir, '/img');
+// ----------------------------------------------------------------------------
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
+if (!fs.existsSync(tempImg)) {
+  fs.mkdirSync(tempImg);
+}
+// ----------------------------------------------------------------------------
+expressApp.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+expressApp.get('/img/:path*', (req, res) => {
+  res.sendFile(`${tempImg}/${req.params.path}${req.params[0]}`);
+});
+expressApp.listen(3002, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+// ----------------------------------------------------------------------------
